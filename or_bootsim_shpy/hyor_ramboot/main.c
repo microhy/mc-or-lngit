54cc1d921c44f3eaaed5e6c4a36534974321d988
root <root@openrisc-VirtualBox.(none)>
2017-01-08 08:10:40 +0100

    edit Makefile for use bin2vmem in sw/utils/

--- or_bootsim_shpy/hyor_ramboot/main.c
+++ or_bootsim_shpy/hyor_ramboot/main.c
@@ -1,13 +1,29 @@
-//#include<stdio.h>
+#include<stdio.h>
 
 volatile unsigned long timestamp = 0;
 
+
+#define   RAM_MEM_SIZE       (0x400)
+#define   RAM_ADDR_START     (0x1FFF-RAM_MEM_SIZE+1)
+#define   RAM_ADDR_END       (0x1FFF)
+
+typedef unsigned int u32;
+typedef signed int   s32;
+
 int main(void)
 {
+
     int i=0;
-    int j=15;
-    ++i;
-    --j;
+	u32 *ptr_ram=NULL;
+
+	ptr_ram =(u32 *)RAM_ADDR_START;
+
+    for(i=0; i<30; ++i)
+	{
+		*ptr_ram = 0x66660000;
+		ptr_ram++;
+	}
+     
     while(1);
     return 0;
 }
