4c79c0be030b2ce6f3363c225931b6628c2f2e4c
microhy <microhy@opencores@.org>
2017-03-22 18:21:41 +0800

    rm atlys_flash/ \ *.o *.elf *.bin

--- atlys_flash/gitadd_allput.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-git add *
-git commit -m="add ram.ld reset.S for debug, and run ok on atlys"
-git push
--- atlys_flash/main_prtstr.c
+++ atlys_flash/main_prtstr.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/*
+** debug the uart function :
+    uart_print_str()
+**
+**
+**
+*/
 //#define   DBG_OUT_FILE  
 //#define   DP_EN
 #define   UART_EN
@@ -18,14 +25,14 @@
 
 volatile unsigned long timestamp = 0;
 #ifdef STDATA_EN
-	#define Struct_Section __attribute__((unused, section(".stdata_mem_type"),aligned(4)))
+    #define Struct_Section __attribute__((unused, section(".stdata_mem_type"),aligned(4)))
 
-	typedef  struct {
-		char ch[4];
-		unsigned int addrinit;
-		unsigned int len;
-	}STDATA_MEM_TYPE;
-	STDATA_MEM_TYPE arry_dynamic Struct_Section;
+    typedef  struct {
+        char ch[4];
+        unsigned int addrinit;
+        unsigned int len;
+    }STDATA_MEM_TYPE;
+    STDATA_MEM_TYPE arry_dynamic Struct_Section;
 #endif
 
 
@@ -36,51 +43,51 @@ unsigned int g_pixel[M][M]={{0},{0}};
 
 void board_init()
 {
-	uart_init();
-	uart_putc('y');
-	uart_putc('\n');
-	uart_put_num(123455);
-	uart_putc('\n');
-	uart_print_str("----board init ok!\n");
+    uart_init();
+    uart_putc('y');
+    uart_putc('\n');
+    uart_put_num(123455);
+    uart_putc('\n');
+    uart_print_str("----board init ok!\n");
 }
 
 void int_error()
 {
-	uart_print_str("<isr> int_error\n");
-	while(1);
+    uart_print_str("<isr> int_error\n");
+    while(1);
 }
 
 void int_main()
 {
-	uart_print_str("<isr> int_main\n");
-	while(1);
+    uart_print_str("<isr> int_main\n");
+    while(1);
 }
 
 int main()
 {
-	int i,j;
-	unsigned int b_num[M];
+    int i,j;
+    unsigned int b_num[M];
 
-	board_init();
-	
-	for(i=0; i<M; i++)
-	{
-		b_num[i] = i;
-		for(j=0; j<M; j++)
-		{
-			g_pixel[i][j]=i*j;
-		}
-	}
-	for(i=0; i<M; i++)
-	{
-		for(j=0; j<M; j++)
-		{
-			uart_put_num(g_pixel[i][j]);
-			uart_putc('\t');
-		}
-		uart_putc('\n');
-	}
+    board_init();
+    
+    for(i=0; i<M; i++)
+    {
+        b_num[i] = i;
+        for(j=0; j<M; j++)
+        {
+            g_pixel[i][j]=i*j;
+        }
+    }
+    for(i=0; i<M; i++)
+    {
+        for(j=0; j<M; j++)
+        {
+            uart_put_num(g_pixel[i][j]);
+            uart_putc('\t');
+        }
+        uart_putc('\n');
+    }
 
-	uart_print_str("----main exit\n");
-	return 0;
+    uart_print_str("----main exit\n");
+    return 0;
 }
